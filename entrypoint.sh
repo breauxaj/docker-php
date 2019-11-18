0bee921aa68e4f90efb57183083dd3bc49941d39
Adam Breaux <adam.breaux@gopuff.com>
2019-11-18 10:46:50 -0500

    Fixed the sed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+APACHE_DOCUMENT_ROOT=/usr/src/app
 APACHE_SERVER_NAME=localhost
 
 rm -f /run/apache2/apache2.pid
 rm -f /run/apache2/httpd.pid
 
 sed -i "s/#ServerName www.example.com:80/ServerName $APACHE_SERVER_NAME/" /etc/apache2/httpd.conf
-sed -i "s/DocumentRoot \"/var/www/localhost/htdocs\"/DocumentRoot /usr/src/app" /etc/apache2/httpd.conf
+sed -i "s#/var/www/localhost/htdocs#$APACHE_DOCUMENT_ROOT#g" /etc/apache2/httpd.conf
 
 httpd -D FOREGROUND
