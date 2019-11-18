d7663e7db6ffddea84f1a8ce1d990596979d195d
Adam Breaux <adam.breaux@gopuff.com>
2019-11-18 10:31:37 -0500

    change the document root path

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ rm -f /run/apache2/apache2.pid
 rm -f /run/apache2/httpd.pid
 
 sed -i "s/#ServerName www.example.com:80/ServerName $APACHE_SERVER_NAME/" /etc/apache2/httpd.conf
+sed -i "s/DocumentRoot \"/var/www/localhost/htdocs\"/DocumentRoot /usr/src/app" /etc/apache2/httpd.conf
 
 httpd -D FOREGROUND
